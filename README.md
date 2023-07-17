670112354a49f9f10664bfe54d3cc5b4893a08d3
Marc Schär <admin@admins-MacBook-Pro.local>
2023-07-17 22:26:28 +0200

    alpha

---
+++ README.md
@@ -0,0 +1,3 @@
+# **Flask Feedback**
+
+In this exercise, we’ll be creating an application that lets users sign up and log in to their own accounts. Once logged in, users can add feedback, edit their feedback, delete their feedback, and see a list of all feedback that they’ve given. Many of these routes should be protected, so that for example *user1* can’t edit a piece of feedback that *user2* created.
---
+++ app.py
@@ -0,0 +1,30 @@
+"""Flask app"""
+from flask import Flask, render_template, redirect, request, flash, session, jsonify
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db 
+# from flask_cors import CORS
+
+# Create a FLASK instance
+app = Flask(__name__)
+ # This will enable CORS for all routes of your app.
+# CORS(app)
+# Add a DATABASE
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+# SECRET KEY
+app.config['SECRET_KEY'] = "hyptokrypo"
+# DEBUG TOOLBAR
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+# initializes the Flask Debug Toolbar
+debug = DebugToolbarExtension(app)
+# connect to DATABASE
+# connect_db(app)
+
+# with app.app_context():
+#     db.drop_all()
+#     db.create_all()
+    # db.session.add_all([c1, c2])
+    # db.session.commit()
+
+#----- ROUTES -----#
